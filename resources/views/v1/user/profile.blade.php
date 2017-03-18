2cf24d1bcdc5b7e975e546df48a2cea5f87844c3
Cuthbert Mirambo <miracuthbert@gmail.com>
2017-03-19 01:20:00 +0300

    Add User Profile Avatar Views

--- resources/views/v1/user/profile.blade.php
+++ resources/views/v1/user/profile.blade.php
@@ -15,7 +15,8 @@
 
                     <p class="lead">Change your personal details or your password here.</p>
 
-                    <form name="user-profile-form" method="post" action="{{ route('user.profile.update') }}" enctype="application/x-www-form-urlencoded"
+                    <form name="user-profile-form" method="post" action="{{ route('user.profile.update') }}"
+                          enctype="application/x-www-form-urlencoded"
                           autocomplete="off">
                         @include('includes.alerts.default')
 
@@ -24,6 +25,40 @@
                         {{ csrf_field() }}
 
                         <input type="hidden" name="id" id="id" value="{{ Auth::user()->id }}">
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading clearfix">
+                                        <img src="{{ url('images/site/logos/thumbs/default.jpg') }}"
+                                             alt="{{ Auth::user()->firstname }} avatar" class="img-thumbnail">
+                                        Last changed: {{ 'a month ago' }}
+
+                                        <div class="pull-right">
+                                            <div class="btn-group btn-group-sm">
+                                                <a href="#avatar" class="btn btn-default">
+                                                    <i class="fa fa-file-image-o"></i>
+                                                    Change Logo
+                                                </a>
+                                            </div>
+                                            <div class="btn-group btn-group-sm">
+                                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                                                    Actions
+                                                    <i class="caret"></i>
+                                                </button>
+                                                <ul class="dropdown-menu pull-right">
+                                                    <li>
+                                                        <a href="#">Set Default</a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <hr>
 
                         <div class="row">
                             <div class="col-md-6">
@@ -86,7 +121,8 @@
                                     <p class="form-static-control">
                                         {{ Auth::user()->activated == 1 ? 'Verified' : '' }}
                                         @if(Auth::user()->activated == 0)
-                                            <a href="{{ route('activation.key.resend') }}" class="btn btn-primary btn-sm">
+                                            <a href="{{ route('activation.key.resend') }}"
+                                               class="btn btn-primary btn-sm">
                                                 Get verified
                                             </a>
                                         @endif
