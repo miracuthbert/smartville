9918ac95a61dc3603256c3c2e84ff1804e150616
miracuthbert <miracuthbert@gmail.com>
2017-05-02 20:07:10 +0300

    Update Notification Helpers
    Updated old helper functions
    Added new helper functions (NotificationUserRoute and
    NotificationEstateRoute)

--- app/Helpers/NotificationHelpers.php
+++ app/Helpers/NotificationHelpers.php
@@ -3,7 +3,7 @@
 //Notification Read
 function ToggleRead($date)
 {
-    if($date == null)
+    if ($date == null)
         return 'Mark as read';
     else
         return 'Marked as read';
@@ -12,7 +12,7 @@ function ToggleRead($date)
 //Notification Read
 function ToggleButtonRead($date)
 {
-    if($date == null)
+    if ($date == null)
         return 'btn-success';
     else
         return 'btn-default disabled';
@@ -21,30 +21,32 @@ function ToggleButtonRead($date)
 //Notification Icon
 function NotificationIcon($type)
 {
-    if($type == 'contact')
+    if ($type == 'contact')
         return 'fa-envelope';
-    if($type == 'forum')
+    if ($type == 'forum')
         return 'fa-comment';
-    if($type == 'bug')
+    if ($type == 'bug')
         return 'fa-bug';
-    if($type == 'tenant bill')
+    if ($type == 'tenant bill')
         return 'fa-money';
-    if($type == 'subscription')
+    if ($type == 'subscription')
         return 'fa-credit-card-alt';
-    if($type == 'create_bill_invoices')
+    if ($type == 'create_bill_invoices' || $type == 'tenant_bill_invoice')
         return 'fa-credit-card';
-    if($type == 'pending_bills_invoices')
+    if ($type == 'pending_bills_invoices')
         return 'fa-money';
-    if($type == 'pending_rent_invoices')
+    if ($type == 'pending_rent_invoices' || $type == 'tenant_rent_invoice')
         return 'fa-credit-card-alt';
+    if ($type == 'activated_tenancy')
+        return 'fa-edit';
 }
 
 //Notification Type
 function NotificationType($type)
 {
-    if($type == 'contact')
+    if ($type == 'contact')
         return true;
-    if($type == 'forum')
+    if ($type == 'forum')
         return true;
 
     return false;
@@ -53,10 +55,77 @@ function NotificationType($type)
 //Notification Type
 function NotificationRoute($type)
 {
-    if($type == 'contact')
+    if ($type == 'contact')
         return 'admin.contact.message';
-    if($type == 'forum')
+    if ($type == 'forum')
         return 'forum.show';
-    if($type == 'bug')
+    if ($type == 'bug')
         return 'bugs.show';
+}
+
+function NotificationUserRoute($notification, $user)
+{
+    $type = $notification->data['type'];
+
+    if ($type == 'subscription') {
+        return '#';
+    } elseif ($type == 'forum') {    //create bill invoice route
+        if ($notification->read_at != null) { //read route
+            return route('forum.show', ['id' => $notification->data['id'], '#comment'.$notification->data['comment_id'], 'read' => $notification->id]);
+        } else {    //not read route
+            return route('forum.show', ['id' => $notification->data['id'], '#comment'.$notification->data['comment_id'], 'notify' => $notification->id]);
+        }
+    } elseif ($type == 'tenant_bill_invoice') {  //tenant bill invoice route
+        if ($notification->read_at != null) { //read route
+            return route('tenant.bill', ['id' => $notification->data['invoice_id']]);
+        } else {    //not read route
+            return route('tenant.bill', ['id' => $notification->data['invoice_id'], 'read' => $notification->id]);
+        }
+    } elseif ($type == 'tenant_rent_invoice') {  //tenant rent invoice route
+        if ($notification->read_at != null) { //read route
+            return route('tenant.rent', ['id' => $notification->data['rent_id']]);
+        } else {    //not read route
+            return route('tenant.rent', ['id' => $notification->data['rent_id'], 'read' => $notification->id]);
+        }
+    } elseif ($type == 'activated_tenancy') {  //activated tenancy route
+        if ($notification->read_at != null) { //read route
+            return route('tenant.dashboard', ['id' => $notification->data['tenant_id']]);
+        } else {    //not read route
+            return route('tenant.dashboard', ['id' => $notification->data['tenant_id'], 'read' => $notification->id]);
+        }
+    }
+}
+
+/**
+ * Handles Notification Route
+ *
+ * @param $notification
+ * @param $app
+ * @return string
+ */
+function NotificationEstateRoute($notification, $app)
+{
+    $type = $notification->data['type'];
+
+    if ($type == 'subscription') {
+        return '#';
+    } elseif ($type == 'create_bill_invoices') {    //create bill invoice route
+        if ($notification->read_at != null) { //read route
+            return route('estate.rental.bills.generate', ['id' => $app->id, 'service' => $notification->data['billing_id']]);
+        } else {    //not read route
+            return route('estate.rental.bills.generate', ['id' => $app->id, 'service' => $notification->data['billing_id'], 'notify' => $notification->id]);
+        }
+    } elseif ($type == 'pending_bills_invoices') {  //pending bills route
+        if ($notification->read_at != null) {   //read route
+            return route('estate.rental.bills.tenants', ['id' => $app->id, 'sort' => 'pending', 'service' => $notification->data['billing_id'], 'today' => $notification->data['is_today'], 'date' => $notification->created_at->toDateString()]);
+        } else {    //not read route
+            return route('estate.rental.bills.tenants', ['id' => $app->id, 'sort' => 'pending', 'service' => $notification->data['billing_id'], 'notify' => $notification->id, 'today' => $notification->data['is_today'], 'date' => $notification->created_at->toDateString()]);
+        }
+    } elseif ($type == 'pending_rent_invoices') { //pending rent routes
+        if ($notification->read_at != null) { //notification read route
+            return route('estate.rental.rents', ['id' => $app->id, 'sort' => 'pending', 'today' => $notification->data['is_today'], 'date' => $notification->created_at->toDateString()]);
+        } else {    //not read route
+            route('estate.rental.rents', ['id' => $app->id, 'sort' => 'pending', 'notify' => $notification->id, 'today' => $notification->data['is_today'], 'date' => $notification->created_at->toDateString()]);
+        }
+    }
 }
