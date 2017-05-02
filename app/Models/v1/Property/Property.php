665e867da0423d5d8b664bdecc16236781c4bbeb
miracuthbert <miracuthbert@gmail.com>
2017-05-02 22:34:35 +0300

    Update Property Model
    Added group relation
    Added features relation
    Added amenities relation

--- app/Models/v1/Property/Property.php
+++ app/Models/v1/Property/Property.php
@@ -2,9 +2,34 @@
 
 namespace App\Models\v1\Property;
 
+use App\Models\v1\Estate\EstateGroup;
 use Illuminate\Database\Eloquent\Model;
 
 class Property extends Model
 {
-    //
+    /**
+     * Get property's group
+     */
+    public function group()
+    {
+        return $this->belongsTo(EstateGroup::class, 'property_group', 'id');
+    }
+
+    /**
+     * Get property's group
+     */
+    public function features()
+    {
+        return $this->hasMany(PropertyFeature::class, 'property_id', 'id');
+    }
+
+    /**
+     * Get property's amenities
+     */
+    public function amenities()
+    {
+        return $this->hasMany(PropertyAmenity::class, 'property_id', 'id');
+    }
+
+
 }
