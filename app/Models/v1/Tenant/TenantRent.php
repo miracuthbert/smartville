f2268e6f40ba14b2d6a6a0de53c3cda4d58e1f2f
miracuthbert <miracuthbert@gmail.com>
2017-05-03 20:42:34 +0300

    Update Model TenantRent
    Added dates accessors

--- app/Models/v1/Tenant/TenantRent.php
+++ app/Models/v1/Tenant/TenantRent.php
@@ -4,8 +4,10 @@ namespace App\Models\v1\Tenant;
 
 use App\Models\v1\Company\CompanyApp;
 use App\Models\v1\Estate\EstateProperty;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use ExtCountries;
 
 class TenantRent extends Model
 {
@@ -16,7 +18,224 @@ class TenantRent extends Model
     /**
      * @var array $dates
      */
-    protected $dates = ['created_at', 'updated_at', 'deleted_at', 'date_from', 'date_to', 'date_due'];
+    protected $dates = ['created_at', 'updated_at', 'deleted_at', 'date_from', 'date_to', 'date_due', 'paid_at'];
+
+    /**
+     * @param $date
+     * @return string
+     */
+    public function getCreatedAtAttribute($date)
+    {
+        $company = $this->property->app->company;
+        $country = $company->country;
+        $state = $company->state;
+
+        $ctz = ExtCountries::where('name.common', $country)->first()->timezone;
+
+        if (count($ctz) > 1) {
+            //get states
+            $states = ExtCountries::where('name.common', $country)->first()->states->pluck('name', 'postal');
+
+            //find state
+            $state = collect($states)->search($state);
+
+            $ctz = $ctz[$state];
+        }
+
+        if ($date != null) {
+            $date = Carbon::parse($date);
+            $date = $date->setTimezone($ctz);
+        }
+
+        return $date;
+
+    }
+
+    /**
+     * @param $date
+     * @return string
+     */
+    public function getUpdatedAtAttribute($date)
+    {
+        $company = $this->property->app->company;
+        $country = $company->country;
+        $state = $company->state;
+
+        $ctz = ExtCountries::where('name.common', $country)->first()->timezone;
+
+        if (count($ctz) > 1) {
+            //get states
+            $states = ExtCountries::where('name.common', $country)->first()->states->pluck('name', 'postal');
+
+            //find state
+            $state = collect($states)->search($state);
+
+            $ctz = $ctz[$state];
+        }
+
+        if ($date != null) {
+            $date = Carbon::parse($date);
+            $date = $date->setTimezone($ctz);
+        }
+
+        return $date;
+
+    }
+
+    /**
+     * @param $date
+     * @return string
+     */
+    public function getDeletedAtAttribute($date)
+    {
+        $company = $this->property->app->company;
+        $country = $company->country;
+        $state = $company->state;
+
+        $ctz = ExtCountries::where('name.common', $country)->first()->timezone;
+
+        if (count($ctz) > 1) {
+            //get states
+            $states = ExtCountries::where('name.common', $country)->first()->states->pluck('name', 'postal');
+
+            //find state
+            $state = collect($states)->search($state);
+
+            $ctz = $ctz[$state];
+        }
+
+        if ($date != null) {
+            $date = Carbon::parse($date);
+            $date = $date->setTimezone($ctz);
+        }
+
+        return $date;
+
+    }
+
+    /**
+     * @param $date
+     * @return string
+     */
+    public function getPaidAtAttribute($date)
+    {
+        $company = $this->property->app->company;
+        $country = $company->country;
+        $state = $company->state;
+
+        $ctz = ExtCountries::where('name.common', $country)->first()->timezone;
+
+        if (count($ctz) > 1) {
+            //get states
+            $states = ExtCountries::where('name.common', $country)->first()->states->pluck('name', 'postal');
+
+            //find state
+            $state = collect($states)->search($state);
+
+            $ctz = $ctz[$state];
+        }
+
+        if ($date != null) {
+            $date = Carbon::parse($date);
+            $date = $date->setTimezone($ctz);
+        }
+
+        return $date;
+
+    }
+
+    /**
+     * @param $date
+     * @return string
+     */
+    public function getDateDueAttribute($date)
+    {
+        $company = $this->property->app->company;
+        $country = $company->country;
+        $state = $company->state;
+
+        $ctz = ExtCountries::where('name.common', $country)->first()->timezone;
+
+        if (count($ctz) > 1) {
+            //get states
+            $states = ExtCountries::where('name.common', $country)->first()->states->pluck('name', 'postal');
+
+            //find state
+            $state = collect($states)->search($state);
+
+            $ctz = $ctz[$state];
+        }
+
+        if ($date != null) {
+            $date = Carbon::parse($date);
+            $date = $date->setTimezone($ctz);
+        }
+
+        return $date;
+
+    }
+
+    /**
+     * @param $date
+     * @return string
+     */
+    public function getDateFromAttribute($date)
+    {
+        $company = $this->property->app->company;
+        $country = $company->country;
+        $state = $company->state;
+
+        $ctz = ExtCountries::where('name.common', $country)->first()->timezone;
+
+        if (count($ctz) > 1) {
+            //get states
+            $states = ExtCountries::where('name.common', $country)->first()->states->pluck('name', 'postal');
+
+            //find state
+            $state = collect($states)->search($state);
+
+            $ctz = $ctz[$state];
+        }
+
+        if ($date != null) {
+            $date = Carbon::parse($date);
+            $date = $date->setTimezone($ctz);
+        }
+
+        return $date;
+
+    }
+
+    /**
+     * @param $date
+     * @return string
+     */
+    public function getDateToAttribute($date)
+    {
+        $company = $this->property->app->company;
+        $country = $company->country;
+        $state = $company->state;
+
+        $ctz = ExtCountries::where('name.common', $country)->first()->timezone;
+
+        if (count($ctz) > 1) {
+            //get states
+            $states = ExtCountries::where('name.common', $country)->first()->states->pluck('name', 'postal');
+
+            //find state
+            $state = collect($states)->search($state);
+
+            $ctz = $ctz[$state];
+        }
+
+        if ($date != null) {
+            $date = Carbon::parse($date);
+            $date = $date->setTimezone($ctz);
+        }
+
+        return $date;
+
+    }
 
     /**
      * Get Rent App
