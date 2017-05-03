fd7752901d68e089d33af25c75066e9abd163cb4
miracuthbert <miracuthbert@gmail.com>
2017-05-03 21:35:04 +0300

    Update TenantBilledNotification
    Added new fields
    Updated comments
    Updated constructor
    Update toMail, toArray methods

--- app/Notifications/Tenant/TenantBilledNotification.php
+++ app/Notifications/Tenant/TenantBilledNotification.php
@@ -21,10 +21,48 @@ class TenantBilledNotification extends Notification
      */
     protected $bill;
 
+
     /**
-     * @var $company
+     * Holds app details
+     *
+     * @var $app
+     */
+    protected $app;
+
+    /**
+     * Holds company details
+     *
+     * @var $company details
      */
     protected $company;
+
+    /**
+     * Holds user details
+     *
+     * @var $user
+     */
+    protected $user;
+
+    /**
+     * Holds notification message
+     *
+     * @var $message
+     */
+    protected $message;
+
+    /**
+     * Holds user route
+     *
+     * @var $userRoute
+     */
+    protected $route;
+
+    /**
+     * Holds notification subject
+     *
+     * @var string $subject
+     */
+    protected $subject;
 
     /**
      * Create a new notification instance.
@@ -40,12 +78,16 @@ class TenantBilledNotification extends Notification
         $this->bill = $bill;
 
         $this->company = $company;
+
+        $this->subject = 'Invoice for ' . $this->bill->title . ' Bill';
+
+        $this->message = $company->title . " has sent you an invoice for rental of property " . $this->property->title . " from " . MonthName($invoice->date_from) . " to " . MonthName($invoice->date_to) . ". Payment is due by " . $invoice->date_due->toDateString() . ".";
     }
 
     /**
      * Get the notification's delivery channels.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return array
      */
     public function via($notifiable)
@@ -56,7 +98,7 @@ class TenantBilledNotification extends Notification
     /**
      * Get the mail representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
@@ -64,15 +106,15 @@ class TenantBilledNotification extends Notification
 //        $user = $notifiable->
 
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', 'https://laravel.com')
-                    ->line('Thank you for using our application!');
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', 'https://laravel.com')
+            ->line('Thank you for using our application!');
     }
 
     /**
      * Get the array representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return array
      */
     public function toArray($notifiable)
@@ -90,11 +132,12 @@ class TenantBilledNotification extends Notification
         $total = $this->bill->bill_plan == 0 ? $amount : ($amount * ($curr - $prev));
 
         return [
-            'title' => $this->bill->title,
+            'title' => $this->subject,
+            'message' => $this->message,
             'from' => $this->company->title,
-            'type' => 'tenant bill',
+            'type' => 'tenant_bill_invoice',
             'invoice_id' => $this->invoice->id,
-            'invoice_due' => $this->invoice->date_due,
+            'invoice_due' => $this->invoice->date_due->toDateString(),
             'amount' => $total,
             'company_app_id' => $this->bill->app->id,
         ];
