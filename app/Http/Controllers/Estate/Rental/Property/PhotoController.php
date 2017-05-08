3943f51f1220c336e5cb0672a073bb7a0dcab130
miracuthbert <miracuthbert@gmail.com>
2017-05-08 19:55:57 +0300

    Update Estate\Rental\properties\PhotoController
    Added functionality to 'create' method

--- app/Http/Controllers/Estate/Rental/Property/PhotoController.php
+++ app/Http/Controllers/Estate/Rental/Property/PhotoController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Estate\Rental\Property;
 
+use App\Models\Image\Gallery;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use ExtCountries;
+use Image;
 
 class PhotoController extends Controller
 {
@@ -22,9 +25,44 @@ class PhotoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request, $id)
     {
-        //
+        //gallery
+        $gallery = Gallery::find($id);
+
+        //Property
+        $property = $gallery->galleryable;
+
+        //check app
+        if ($property == null)
+            abort(404);
+
+        //Property App
+        $app = $property->app;
+
+        //company
+        $company = $app->company;
+
+        //country code
+        $code = ExtCountries::where('name.common', $company->country)->first()->callingCode[0];
+
+        //currency sign or iso code
+        $currency = ExtCountries::where('name.common', $company->country)->first()->currency;
+        $currency = $currency[0]['sign'] != null ? $currency[0]['sign'] : $currency[0]['ISO4217Code'];
+
+        //authorize
+        $this->authorize('view', $app);
+
+        //TODO: use for debug only
+        //dd($property->prices()->where('status',1)->first());
+
+        return view('v1.estates.properties.photos.create')
+            ->with('app', $app)
+            ->with('code', $code)
+            ->with('currency', $currency)
+            ->with('company', $company)
+            ->with('property', $property)
+            ->with('gallery', $gallery);
     }
 
     /**
