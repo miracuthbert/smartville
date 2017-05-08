82ede6a9bded6e5b3d5de0a49e49d65550b45022
miracuthbert <miracuthbert@gmail.com>
2017-05-08 19:37:25 +0300

    Update Image Models
    Added polymorphic relation to Photo Model
    Added 'guarded' and 'dates' field to Photo Model
    Added photos relation to Gallery Model
    Updated comment in Gallery Model polymorphic function

--- app/Models/Image/Gallery.php
+++ app/Models/Image/Gallery.php
@@ -10,8 +10,23 @@ class Gallery extends Model
 
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 
+    /**
+     * Get all of the owning galleryable models.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
     public function galleryable()
     {
         return $this->morphTo();
+    }
+
+    /**
+     * Get all photos' of gallery
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    public function photos()
+    {
+        return $this->morphMany(Photo::class, 'photoable');
     }
 }
--- app/Models/Image/Photo.php
+++ app/Models/Image/Photo.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
-    //
+    protected $guarded = [];
+
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    /**
+     * Get all of the owning photoable models.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function photoable()
+    {
+        return $this->morphTo();
+    }
 }
