5f82ba2a11aaa391d784a4de0f2026c5c0418dc1
miracuthbert <miracuthbert@gmail.com>
2017-05-09 22:17:58 +0300

    Update Photo Model in Models\Image
    Added 'audience' relation to model
    Added 'casts' field to model

--- app/Models/Image/Photo.php
+++ app/Models/Image/Photo.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Image;
 
+use App\Models\v1\Shared\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
@@ -9,6 +10,10 @@ class Photo extends Model
     protected $guarded = [];
 
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    protected $casts = [
+        'data' => 'array',
+    ];
 
     /**
      * Get all of the owning photoable models.
@@ -18,5 +23,15 @@ class Photo extends Model
     public function photoable()
     {
         return $this->morphTo();
+    }
+
+    /**
+     * Get photo audience details
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function audience()
+    {
+        return $this->belongsTo(Category::class, 'audience_id', 'id');
     }
 }
