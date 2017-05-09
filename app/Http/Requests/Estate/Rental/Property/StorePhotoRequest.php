1bbc9d4026cefd3c6cbb728a6b25071b6a2bd8f8
miracuthbert <miracuthbert@gmail.com>
2017-05-09 22:23:24 +0300

    Add StorePhotoRequest to requests
    Added a request file to handle for validation of estate\rental\property
    photos

---
+++ app/Http/Requests/Estate/Rental/Property/StorePhotoRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Estate\Rental\Property;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePhotoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'gallery' => 'required|exists:galleries,id',
+            'caption' => 'required|min:2|max:160',
+            'description' => 'min:50|max:255',
+            'photo' => 'required|image|mimes:jpeg,jpg,png|dimensions:min_width=800,min_height=600',
+            'audience' => 'required|integer',
+            'status' => 'required|boolean',
+        ];
+    }
+}
