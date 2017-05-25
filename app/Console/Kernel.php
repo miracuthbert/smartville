1e1505ed0d98cfb888293b3b4fa154b3c1716819
miracuthbert <miracuthbert@gmail.com>
2017-05-26 00:15:41 +0300

    Update console 'kernel' schedules
    Assigned schedules correctly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,43 +33,47 @@ class Kernel extends ConsoleKernel
 
         //schedule app daily tasks
         $schedule->call(function () {
-            AppBillingHandler::billsDueReminder();          //app bills due reminder
-            AppBillingHandler::billsPastDueReminder();      //app bills past due reminder
-            AppBillingHandler::billingDayReminder();        //app billing day reminder
-            AppRentalHandler::rentDueReminder();            //app rents due reminder
-            AppRentalHandler::rentPastDueReminder();        //app rents past due reminder
+            #TODO: DO NOT ADD SCHEDULES THAT DON'T NEED TO BE RUN DAILY - (EARLY)
+//            AppBillingHandler::billsDueReminder();          //app bills due reminder
+//            AppBillingHandler::billsPastDueReminder();      //app bills past due reminder
+//            AppBillingHandler::billingDayReminder();        //app billing day reminder
+//            AppRentalHandler::rentDueReminder();            //app rents due reminder
+//            AppRentalHandler::rentPastDueReminder();        //app rents past due reminder
         })->daily();
 
         //schedule app daily tasks at 13 hrs
         $schedule->call(function () {
-            AppBillingHandler::billsDueReminder();          //app bills due reminder
-            AppBillingHandler::billsPastDueReminder();      //app bills past due reminder
-            AppBillingHandler::billingDayReminder();        //app billing day reminder
-            AppRentalHandler::rentDueReminder();            //app rents due reminder
-            AppRentalHandler::rentPastDueReminder();        //app rents past due reminder
+            #TODO: ONLY IMPORTANT DAILY 'AFTER NOON/LUNCH' SCHEDULES SHOULD BE PLACED HERE
+//            AppBillingHandler::billsDueReminder();          //app bills due reminder
+//            AppBillingHandler::billsPastDueReminder();      //app bills past due reminder
+//            AppBillingHandler::billingDayReminder();        //app billing day reminder
+//            AppRentalHandler::rentDueReminder();            //app rents due reminder
+//            AppRentalHandler::rentPastDueReminder();        //app rents past due reminder
         })->dailyAt('13:00');
 
         //schedule app daily tasks at 16 hrs
         $schedule->call(function () {
-            AppBillingHandler::billsDueReminder();          //app bills due reminder
-            AppBillingHandler::billsPastDueReminder();      //app bills past due reminder
-            AppBillingHandler::billingDayReminder();        //app billing day reminder
-            AppRentalHandler::rentDueReminder();            //app rents due reminder
-            AppRentalHandler::rentPastDueReminder();        //app rents past due reminder
+            #TODO: ONLY IMPORTANT DAILY 'RUSH HOUR' SCHEDULES SHOULD BE PLACED HERE
+//            AppBillingHandler::billsDueReminder();          //app bills due reminder
+//            AppBillingHandler::billsPastDueReminder();      //app bills past due reminder
+//            AppBillingHandler::billingDayReminder();        //app billing day reminder
+//            AppRentalHandler::rentDueReminder();            //app rents due reminder
+//            AppRentalHandler::rentPastDueReminder();        //app rents past due reminder
         })->dailyAt('16:00');
 
         //schedule app tasks twice daily at 6 and 9
         $schedule->call(function () {
+            #TODO: DO NOT ADD SCHEDULES That Do Not Need To Be Run At The Specified Times
             AppBillingHandler::billsDueReminder();          //app bills due reminder
             AppBillingHandler::billsPastDueReminder();      //app bills past due reminder
             AppBillingHandler::billingDayReminder();        //app billing day reminder
             AppRentalHandler::rentDueReminder();            //app rents due reminder
             AppRentalHandler::rentPastDueReminder();        //app rents past due reminder
-        })->twiceDaily(6,9);
+        })->twiceDaily(7,14);
 
         //schedule app subscription handles
         $schedule->call(function () {
-            #TODO: Comment Lines Below That Do
+            #TODO: DO NOT ADD SCHEDULES BELOW THAT DO NOT NEED TO BE RUN EVERY MINUTE
 //            AppBillingHandler::billsDueReminder();          //app bills due reminder
 //            AppBillingHandler::billsPastDueReminder();      //app bills past due reminder
 //            AppBillingHandler::billingDayReminder();        //app billing day reminder
