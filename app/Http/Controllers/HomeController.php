f075bab6609d0d93903ca4c0f6ae09f8023d4a74
miracuthbert <miracuthbert@gmail.com>
2017-06-01 20:49:21 +0300

    Update Welcome View and Controller
    Updated welcome view
    Pointed the HomeController@index method to the new view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('v1.landing');
+        return view('welcome');
     }
 
     /**
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,120 +1,225 @@
-@extends('layouts.master')
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('title')
-    Welcome!
-@endsection
+    <title>{{ config('app.name') }}</title>
 
-@section('content')
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-    @include('includes.headers.default')
+    <!-- Styles -->
+    <link href="/css/app.css" rel="stylesheet">
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
 
-    @include('includes.alerts.primary')
+        .full-height {
+            display: table;
+            width: 100%;
+            height: 100%;
+            min-height: 100%;
+        }
 
-    <div class="jumbotron bg-green-sea white"><!-- Main jumbotron for a primary marketing message or call to action -->
-        <div class="container">
+        .full-height-inner {
+            display: table-cell;
+            vertical-align: middle;
+        }
 
-            <div class="text-center">
-                <h1>Smart Ville</h1>
+        .position-ref {
+            position: relative;
+        }
 
-                <p>This is a template for a simple marketing or informational website. It includes a large callout
-                    called a jumbotron and three supporting pieces of content. Use it as a starting point to create
-                    something more unique.</p>
-            </div>
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-        </div>
-    </div>
+        .content {
+            padding: 20px;
+            text-align: center;
+        }
 
-    <!-- Main Services -->
-    <section class="section" id="services">
-        <div class="container">
-            <!-- Example row of columns -->
-            <div class="row">
-                <div class="ocl-lg-12 text-center">
-                    <h1 class="sub-header">Products and Services</h1>
-                    <hr>
+        .title {
+            font-size: 84px;
+        }
+
+        .links a, .nav > li > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .nav > li > a {
+            padding: 15px 25px;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+
+        .masthead, .mastfoot, .cover-container {
+            width: 100%;
+        }
+
+        .masthead {
+            position: fixed;
+            top: 0;
+        }
+
+        .masthead {
+            margin-bottom: 2rem;
+        }
+
+        .navbar-default {
+            border-bottom: 0;
+        }
+    </style>
+</head>
+<body>
+<div class="full-height">
+    <div class="full-height-inner">
+        <nav class="navbar navbar-default navbar-static-top masthead">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                            data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Smart Diary') }}
+                    </a>
                 </div>
 
-                <div class="col-md-4">
-                    <section class="section-pad">
-                        <h3>Rental Management System</h3>
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        &nbsp;
+                    </ul>
 
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
-                            tortor
-                            mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
-                            magna
-                            mollis euismod. Donec sed odio dui. </p>
+                    <!-- Right Side Of Navbar -->
+                    @if (Route::has('login'))
+                        <ul class="nav navbar-nav navbar-right">
+                            @if (Auth::guest())
+                                <li class="{{ ActivePage('login') }}">
+                                    <a href="{{ route('login') }}">Login <i class="fa fa-sign-in"></i></a>
+                                </li>
+                                <li class="{{ ActivePage('register') }}">
+                                    <a href="{{ route('register') }}">Sign Up <i class="fa fa-user-plus"></i></a>
+                                </li>
+                            @else
+                                <li>
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                       aria-haspopup="true"
+                                       aria-expanded="false">
+                                        <span class="badge">{{ count($unread_notifications) > 0 ? count($unread_notifications) : '' }}</span>
+                                        {{ Auth::user()->username != null ? Auth::user()->username : Auth::user()->firstname  }}
+                                        <span class="fa fa-user"></span>
+                                        <span class="caret"></span>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a href="{{ route('user.dashboard') }}" title="my dashboard">
+                                                <i class="fa fa-dashboard fa-fw"></i> My dashboard</a>
+                                        </li>
+                                        <!-- user dashboard -->
 
-                        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-                    </section>
-                </div>
-                <div class="col-md-4">
-                    <section class="section-pad">
-                        <h3>Hostel Management System</h3>
+                                        <li>
+                                            <a href="{{ route('user.dashboard', ['section' => 'apps']) }}">
+                                                <i class="fa fa-laptop"></i> My Apps
+                                            </a>
+                                        </li>
+                                        <!-- user apps -->
 
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
-                            tortor
-                            mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
-                            magna
-                            mollis euismod. Donec sed odio dui. </p>
+                                        <li>
+                                            <a href="{{ route('user.notifications') }}" title="notifications">
+                                                <i class="fa fa-bell fa-fw"></i> Notifications <span
+                                                        class="badge">{{ count($unread_notifications) > 0 ? count($unread_notifications) : '' }}</span>
+                                            </a>
+                                        </li>
+                                        <!-- notifications -->
 
-                        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-                    </section>
-                </div>
-                <div class="col-md-4">
-                    <section class="section-pad">
-                        <h3>Property Finder & Booking Service</h3>
+                                        @if(Auth::user()->root or Auth::user()->admin)
+                                            <li>
+                                                <a href="{{ route('admin.dashboard')  }}" title="admin panel">
+                                                    <i class="fa fa-tachometer fa-fw"></i> Admin panel
+                                                </a>
+                                            </li>
+                                            <!-- Admin panel option -->
+                                        @endif
 
-                        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum
-                            id ligula
-                            porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                            condimentum nibh,
-                            ut fermentum massa justo sit amet risus.</p>
+                                        <li>
+                                            <a href="{{ route('user.profile') }}" title="my profile">
+                                                <i class="fa fa-user fa-fw"></i> Profile
+                                            </a>
+                                        </li>
+                                        <!-- profile -->
 
-                        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-                    </section>
-                </div>
-            </div>
-
-            <hr>
-
-        </div> <!-- /container -->
-    </section>
-
-    <!-- Contacts -->
-    <section class="section bg-wet-asphalt white" id="contacts">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="section">
-                        <h3>Location</h3>
-                        <address class="section-lg">
-                            <strong>ClockTower</strong>
-                            <br>
-                            <strong>Arusha, Tanzania</strong>
-                        </address>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="section">
-                        <h3>Social Media</h3>
-                        <ul class="list-inline">
-                            <li><a href="#" class="btn-social btn-outline"><span class="icon icon-facebook"></span></a>
-                            </li>
-                            <li><a href="#" class="btn-social btn-outline"><span class="icon icon-twitter"></span></a>
-                            </li>
-                            <li><a href="#" class="btn-social btn-outline"><span class="icon icon-linkedin"></span></a>
-                            </li>
+                                        <li role="separator" class="divider"></li>
+                                        <li>
+                                            <a href="{{ route('logout') }}"
+                                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                                <i class="fa fa-sign-out fa-fw"></i> Logout
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            @endif
                         </ul>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="section">
-                        <h3>About SmartVille</h3>
-                    </div>
+                    @endif
                 </div>
             </div>
-        </div>
-    </section>
-    @include('includes.footers.default')
+        </nav><!-- /.navbar-default.masthead -->
+        <div class="position-ref">
+            <div class="content">
+                <div class="title m-b-md">
+                    {{ config('app.name') }}
+                </div>
 
-@endsection
+                <div class="nav nav-justified links">
+                    <li>
+                        <a href="{{ route('manuals.index') }}">Documentation</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('services') }}">Apps & Services</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('forum.index') }}">Knowledge Base</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('contact') }}">Contact Us</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('support.index') }}">Support Center</a>
+                    </li>
+                </div>
+            </div><!-- /.content -->
+        </div><!-- /.position-ref -->
+    </div><!-- /.full-height-inner -->
+</div><!-- /.full-height -->
+
+<!-- Scripts -->
+<script src="/js/demo_app.js"></script>
+
+</body><!-- /body -->
+</html>
