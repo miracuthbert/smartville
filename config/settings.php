6959d9cea81bc981fc7606423f35a6e66cafd5d3
miracuthbert <miracuthbert@gmail.com>
2017-06-03 14:12:09 +0300

    Update and Add Site Settings to config\settings
    Added 'site_settings' to config\settings file
    Updated comment blocks and structure for previously added settings

--- config/settings.php
+++ config/settings.php
@@ -1,5 +1,20 @@
 <?php
 return [
+    /**
+     * -------------------------------------------------------------------
+     * Site Settings
+     * Default Site Settings
+     * These settings can be overridden by creating an alt. settings file
+     * Or Using a 'Database'
+     * -------------------------------------------------------------------
+     */
+    'site_settings' => [
+        //no of allowed trials
+        'trials' => 2,
+
+        //no of trial days
+        'trial_days' => 14,
+    ],
 
     /**
      * -------------------------------------------------------------------
@@ -10,23 +25,47 @@ return [
      */
     'send_activation_email' => env('SETTINGS_SEND_ACTIVATION_EMAIL', true),
 
+    /**
+     * -------------------------------------------------------------------
+     * Avatar Storage Path For Users
+     * Holds the initial storage path for user avatar's/profile picture
+     * -------------------------------------------------------------------
+     */
     'avatar_storage_user' => 'images/avatars/users',
 
+    /**
+     * -------------------------------------------------------------------
+     * Avatar Storage Path For Companies
+     * Holds the initial storage path for company avatar's/logo
+     * -------------------------------------------------------------------
+     */
     'avatar_storage_company' => 'images/avatars/companies',
 
+    /**
+     * -------------------------------------------------------------------
+     * Property Gallery Storage Path
+     * Holds the initial storage path for property galleries
+     * -------------------------------------------------------------------
+     */
     'property_storage_gallery' => 'images/galleries/properties',
 
     /**
+     * -------------------------------------------------------------------
      * Rental App
      * Default Settings
+     * Holds default settings for rental app
+     * Push to Database when creating an app
      * Version 1.0
+     * -------------------------------------------------------------------
      */
-    'rental_app' => array(
+    'rental_app' => [
         /**
+         * -------------------------------------------------------------------
          * Layouts
          * properties, tenants
+         * -------------------------------------------------------------------
          */
-        'layouts' => array(
+        'layouts' => [
             //groups layout
             'groups' => 'list',
 
@@ -41,13 +80,15 @@ return [
 
             //bills layout
             'bills' => 'list',
-        ),
+        ],
 
         /**
+         * -------------------------------------------------------------------
          * Notifications
          * web, email, sms
+         * -------------------------------------------------------------------
          */
-        'notifications' => array(
+        'notifications' => [
             //web
             'web' => true,
 
@@ -56,33 +97,35 @@ return [
 
             //sms
             'sms' => false,
-        ),
+        ],
 
         /**
+         * -------------------------------------------------------------------
          * Sort Options
          * properties, tenants
+         * -------------------------------------------------------------------
          */
         //property groups
-        'property_groups_options' => array(
+        'property_groups_options' => [
             'sort' => 'all',
-        ),
+        ],
         //properties
-        'properties_options' => array(
+        'properties_options' => [
             'sort' => 'all',
-        ),
+        ],
         //tenants
-        'tenants_options' => array(
+        'tenants_options' => [
             'sort' => 'all',
             'leases' => false,
-        ),
+        ],
         //rent
-        'rent_options' => array(
+        'rent_options' => [
             'sort' => 'all',
-        ),
+        ],
         //bills
-        'bills_options' => array(
+        'bills_options' => [
             'sort' => 'all',
-        ),
-    ),
+        ],
+    ],
 
 ];
