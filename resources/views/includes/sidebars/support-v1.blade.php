3de7352de9888da67752d625e3f790952207b3c0
miracuthbert <miracuthbert@gmail.com>
2017-06-05 13:19:38 +0300

    Update support sidebar include view
    Update sidebar header
    Update links to show indicate 'current' page

--- resources/views/includes/sidebars/support-v1.blade.php
+++ resources/views/includes/sidebars/support-v1.blade.php
@@ -1,16 +1,18 @@
 <div class="col-lg-3 col-sm-4">
     <div class="list-group">
-        <a href="#" class="list-group-item active">
-            <h4 class="list-group-item-heading">Support Menu</h4>
-            <p class="list-group-item-text">Use links below to skip to sections</p>
-        </a>
-        <a href="{{ route('manuals.index') }}" class="list-group-item">
+        <div class="list-group-item active">
+            <div class="list-group-item-heading">
+                <h4>Support Menu</h4>
+                <p class="list-group-item-text">Use links below to skip to sections</p>
+            </div>
+        </div>
+        <a href="{{ route('manuals.index') }}" class="list-group-item {{ ActivePage('manuals.index') }}">
             <i class="glyphicon glyphicon-book"></i> Documentation
         </a>
-        <a href="{{ route('forum.index') }}" class="list-group-item">
+        <a href="{{ route('forum.index') }}" class="list-group-item {{ ActivePage('forum.index') }}">
             <i class="fa fa-question-circle-o fa-fw"></i> Knowledge Base
         </a>
-        <a href="{{ route('bug.create') }}" class="list-group-item">
+        <a href="{{ route('bug.create') }}" class="list-group-item {{ ActivePage('bug.create') }}">
             <i class="fa fa-bug fa-fw"></i> Report a problem (bug)
         </a>
     </div>
