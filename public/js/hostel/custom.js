5bc0d15cce8f83434685992f00b9f24bfdf2dc95
miracuthbert <miracuthbert@gmail.com>
2017-06-27 20:57:09 +0300

    Add custom.js
    Handles custom functionality for hostel scripts (js)

---
+++ public/js/hostel/custom.js
@@ -0,0 +1,78 @@
+/**
+ * Created by Cuthbert Mirambo on 6/26/2017.
+ *
+ * Holds Custom Scripts For Hostel App
+ */
+/**
+ * Add Feature Generate
+ */
+$(document).on('click', '#btnPropertyFeatureGen', function () {
+    //catch click
+    var $this = $(this);
+
+    //markup
+    var $output = '<fieldset class="form-group">';
+    $output += '<div class="row">';
+    $output += '<div class="col-sm-3">';
+    $output += '<div class="md-form">';
+    $output += '<input type="text" name="feature[]" class="form-control _add_feature">';
+    $output += '<label class="form-control-label">Feature</label>';
+    $output += '</div>';
+    $output += '</div>';
+    $output += '<div class="col-md-5">';
+    $output += '<div class="md-form">';
+    $output += '<input type="text" name="details[]" class="form-control _add_details" maxlength="255">';
+    $output += '<label class="form-control-label">Feature Details</label>';
+    $output += '</div>';
+    $output += '</div>';
+    $output += '<div class="col-md-2">';
+    $output += '<div class="md-form">';
+    $output += '<input type="text" name="value[]" class="form-control _add_value">';
+    $output += '<label class="form-control-label">#</label>';
+    $output += '</div>';
+    $output += '</div>';
+    $output += '<div class="col-md-2">';
+    $output += '<button type="button" name="btnFeatureGen" class="btn btn-warning btn-sm btnPropertyRemoveFeature pull-right"';
+    $output += ' data-toggle="tooltip" title="Remove feature">';
+    $output += 'Remove ';
+    $output += '<span class="fa fa-remove"></span>';
+    $output += '</button>';
+    $output += '</div>';
+    $output += '</div>';
+    $output += '</fielset>';
+
+
+    // $output += '';
+
+    //add markup
+    $($this).before($output);
+
+});
+
+/**
+ * Remove Generated Feature
+ */
+$(document).on('click', '.btnPropertyRemoveFeature', function () {
+    //catch click
+    var $this = $(this);
+
+    //transverse to top parent
+    $this.parent().parent().remove();
+});
+
+/**
+ * On Change Multiple Tenancy
+ */
+$('input[name="multiple_tenants"]').on('change', function () {
+
+    //catch selector
+    $this = $(this);
+
+    //check and change 'input#tenants' property disabled
+    if ($this.val() == 1)
+        $('input#tenants[name="tenants"]').prop('disabled', false);
+    else
+        $('input#tenants[name="tenants"]').prop('disabled', true);
+
+});
+
