4360d3ea0ad45848832275aebba4dccc9492d90d
miracuthbert <miracuthbert@gmail.com>
2017-07-28 14:24:50 +0300

    Update Admin CompanyController
    Added constructor
    Added 'auth' and 'admin' middleware to handle authentication

--- app/Http/Controllers/Admin/Company/CompanyController.php
+++ app/Http/Controllers/Admin/Company/CompanyController.php
@@ -9,6 +9,16 @@ use App\Http\Controllers\Controller;
 class CompanyController extends Controller
 {
     /**
+     * CompanyController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+
+        $this->middleware('admin');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
