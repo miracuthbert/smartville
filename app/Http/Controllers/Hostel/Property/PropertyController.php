2a4059a5adc531b9e0c20335e4647fb83aded019
miracuthbert <miracuthbert@gmail.com>
2017-07-28 15:23:35 +0300

    Update Hostel PropertyController
    Added constructor with 'auth' middleware
    Added functinality to render data for views in create and index methods
    Added functionality ro render views in edit and show methods
    Added custom request parameter in store method

--- app/Http/Controllers/Hostel/Property/PropertyController.php
+++ app/Http/Controllers/Hostel/Property/PropertyController.php
@@ -2,69 +2,91 @@
 
 namespace App\Http\Controllers\Hostel\Property;
 
+use App\Http\Requests\Hostel\Property\StorePropertyRequest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class PropertyController extends Controller
 {
     /**
+     * PropertyController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Display a listing of the resource.
      *
+     * @param Request $request
+     * @param $app
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request, $app)
     {
-        //
+        //find company app
+        $app = CompanyApp::findOrFail($app);
+
+        return view('hostels.properties.index')
+            ->with('app', $app);
     }
 
     /**
      * Show the form for creating a new resource.
      *
+     * @param $app
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create($app)
     {
-        return view('hostels.properties.create');
+        //find company app
+        $app = CompanyApp::findOrFail($app);
+
+        return view('hostels.properties.create')
+            ->with('app', $app);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param StorePropertyRequest|Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StorePropertyRequest $request)
     {
-        //
+        return redirect()->back()
+            ->with('error', "Failed adding property. Please try again!")
+            ->withInput();
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
+        return view('hostels.properties.show');
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        //
+        return view('hostels.properties.edit');
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -75,7 +97,7 @@ class PropertyController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
