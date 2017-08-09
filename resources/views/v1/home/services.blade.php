a8a5ed8d3941e0553b45e3ab3cd85a71d6dc1695
miracuthbert <miracuthbert@gmail.com>
2017-08-10 00:06:57 +0300

    Update services view
    Updated services view layout

--- resources/views/v1/home/services.blade.php
+++ resources/views/v1/home/services.blade.php
@@ -1,28 +1,30 @@
-@extends('v1.layouts.master')
+@extends('layouts.company')
 
 @section('title')
     App & Services
 @endsection
 
 @section('styles')
-    <link href="{{ url('css/v1/home.css') }}" rel="stylesheet">
-@endsection
+    {{--<link href="{{ url('css/v1/home.css') }}" rel="stylesheet">--}}
+    @endsection
 
-@section('content')
+    @section('content')
 
     @include('v1.includes.headers.home.default-static')
 
-    <section class="bg-white box" id="services"><!-- Services Section -->
+    <section class="bg-white box" id="services">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12 text-center">
+                <div class="col-lg-12">
                     <h2 class="section-heading">Apps & Services</h2>
-                    <h3 class="section-subheading text-muted">What We Offer</h3>
                 </div>
             </div>
+
+            <h3 class="page-header"><i class="fa fa-laptop"></i> Apps</h3>
+
             <div class="row text-center" id="apps">
                 @forelse($app_products as $app)
-                    <div class="{{ count($app_products) <= 2 ? 'col-md-12' : 'col-md-6' }}">
+                    <div class="{{ count($app_products) < 3 ? 'col-md-6' : 'col-md-4' }}">
                         <header class="clearfix">
                             <span class="fa-stack fa-4x">
                                 <i class="fa fa-circle fa-stack-2x text-primary"></i>
@@ -40,46 +42,43 @@
                             <!-- /title -->
                         </header>
                         <!-- /header -->
-
+                        <hr>
                         <section class="clearfix summary">
                             {!! $app->summary !!}
                         </section>
                         <!-- /summary -->
+                        <hr>
 
-                        <p class="text-muted">
+                        <div class="text-muted">
                             <a href="{{ route('service', ['id' => $app->id]) }}" class="btn btn-default">
                                 Learn more <i class="fa {{ $app->icon }}"></i>
                             </a>
+
                             <a href="{{ route('app.create', ['app' => str_slug($app->title)]) }}"
                                class="btn btn-primary" data-toggle="tooltip"
                                title="Add {{ $app->title }}">
                                 Get Started
                                 <i class="fa fa-chevron-circle-right"></i>
                             </a>
-                        </p>
+                        </div>
                         <!-- /links -->
 
                         @if($loop->count > 1 && $loop->last)
                             <hr class="hidden-md hidden-lg hidden-sm">
                         @endif
+                    </div><!-- /.col-md-* -->
+                    <p></p>
 
-                        @empty
-                            <p class="text-muted">Sorry, some error occured. Reload page.</p>
-                    </div>
-                    <!-- /.col-md-* -->
+                @empty
+                    <p class="text-muted">Sorry, some error occured. Reload page.</p>
                 @endforelse
-            </div>
-            <!-- /.row -->
+            </div><!-- /.row#apps -->
 
-            <div class="row">
-                <div class="col-lg-12">
-                    <hr>
-                </div>
-            </div>
+            <h3 class="page-header"><i class="fa fa-hourglass-half"></i> Coming Soon</h3>
 
-            <div class="row text-center" id="appsSoon">
+            <div class="row text-center" id="apps-soon">
                 @forelse($apps_coming as $app)
-                    <div class="{{ count($apps_coming) <= 2 ? 'col-md-12' : 'col-md-6' }}">
+                    <div class="{{ count($apps_coming) < 3 ? 'col-md-6' : 'col-md-4' }}">
                         <header>
                             <span class="fa-stack fa-4x">
                                 <i class="fa fa-circle fa-stack-2x text-primary"></i>
@@ -95,11 +94,13 @@
                             <!-- /title -->
                         </header>
                         <!-- /header -->
-
+                        <hr>
+                        
                         <section class="clearfix summary">
                             {!! $app->summary !!}
                         </section>
                         <!-- /summary -->
+                        <hr>
 
                         <p class="text-muted">
                             <a href="{{ route('service', ['id' => $app->id]) }}" class="btn btn-default">
@@ -112,59 +113,16 @@
                             <hr class="hidden-md hidden-lg hidden-sm">
                         @endif
 
-                        @empty
-                            <p class="text-muted">Sorry, some error occured. Reload page.</p>
-                    </div>
-                    <!-- /.col-md-* -->
+                    </div><!-- /.col-md-* -->
+                    <p></p>
+
+                @empty
+                    <p class="text-muted">Sorry, some error occured. Reload page.</p>
                 @endforelse
-            </div>
-            <!-- /.row -->
+            </div><!-- /.row#apps-soon -->
 
-            <!--
-            <div class="row hidden" id="appsHidden">
-                <div class="col-md-4">
-                    <div class="clearfix">
-                        <span class="fa-stack fa-4x">
-                            <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                            <i class="fa fa-building fa-stack-1x fa-inverse"></i>
-                        </span>
-                    </div>
-                    <h4 class="service-heading">Hostel Management System</h4>
-
-                    <p class="text-muted">Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac
-                        cursus
-                        commodo,
-                        tortor
-                        mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
-                        magna
-                        mollis euismod. Donec sed odio dui. </p>
-
-                    <p class="label label-warning">Coming Soon!</p>
-                </div>
-                <div class="col-md-4">
-                    <div class="clearfix">
-                        <span class="fa-stack fa-4x">
-                            <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                            <i class="fa fa-search-plus fa-stack-1x fa-inverse"></i>
-                        </span>
-                    </div>
-                    <h4 class="service-heading">Property Finder & Booking Service</h4>
-
-                    <p class="text-muted">Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget
-                        quam.
-                        Vestibulum
-                        id ligula
-                        porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                        condimentum nibh,
-                        ut fermentum massa justo sit amet risus.</p>
-
-                    <p class="label label-warning">Coming Soon!</p>
-                </div>
-            </div> -->
-        </div>
-        <!-- /.container -->
-    </section>
-    <!-- /#services -->
+        </div><!-- /.container -->
+    </section><!-- /#services -->
 
     @include('includes.footers.default')
 
