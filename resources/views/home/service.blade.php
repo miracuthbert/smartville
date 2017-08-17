603057c338dc0c4b3f1473d3b6495c209fcffd87
miracuthbert <miracuthbert@gmail.com>
2017-08-17 21:16:36 +0300

    Update home dir views and styles
    Updated views layout
    Updated home.css

--- resources/views/home/service.blade.php
+++ resources/views/home/service.blade.php
@@ -1,101 +1,86 @@
-@extends('v1.layouts.master')
+@extends('layouts.company.master')
 
 @section('title')
     Apps & Services - {{ $product->title }}
 @endsection
 
-@section('styles')
-    <link href="{{ url('css/v1/home.css') }}" rel="stylesheet">
-    <style>
-        #product-details {
-            word-wrap: break-word;
-        }
-    </style>
-@endsection
-
 @section('content')
 
-    @include('partials')
+    <div class="section-pad"></div>
 
-    <section class="bg-white clearfix" id="service"><!-- Services Section -->
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2">
-                    <h2 class="page-header">
+    <div class="bg-white clearfix" id="service"><!-- Services Section -->
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2 class="page-header">
                         <span class="fa-stack fa-3x">
                             <i class="fa fa-circle fa-stack-2x text-primary"></i>
                             <i class="fa {{ $product->icon }} fa-stack-1x fa-inverse"></i>
                         </span>
-                        {{ $product->title }}
+                    {{ $product->title }}
 
-                        @if($product->mode != 1)
-                            <small data-toggle="tooltip"
-                                   title="{{ AppModeInfo($product->mode) }}">
-                                <span class="label label-warning">Beta</span>
-                            </small>
-                        @endif
-                    </h2>
+                    @if($product->mode != 1)
+                        <small data-toggle="tooltip"
+                               title="{{ AppModeInfo($product->mode) }}">
+                            <span class="label label-warning">Beta</span>
+                        </small>
+                    @endif
+                </h2>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <div class="clearfix" id="summary">
+                    {!! $product->summary !!}
                 </div>
             </div>
+        </div>
 
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2">
-                    <div class="clearfix" id="summary">
-                        {!! $product->summary !!}
-                    </div>
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <hr>
+                <div id="product-details">
+                    {!! $product->desc !!}
                 </div>
             </div>
+        </div>
 
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2">
-                    <hr>
-                    <div id="product-details">
-                        {!! $product->desc !!}
-                    </div>
-                </div>
-            </div>
-
-            <div class="row hidden">
-                <div class="col-lg-8 col-lg-offset-2">
-                    <h3 class="section-heading">Features</h3>
-                    <hr>
-                    <div id="product-features text-center">
-                        @foreach($product_features->chunk(3) as $features)
-                            <div class="row">
-                                @foreach($features as $feature)
-                                    <div class="col-md-12">
-                                        <dt>
-                                            {{ $feature->feature }}
-                                            <span class="pull-right">
+        <div class="row hidden">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h3 class="section-heading">Features</h3>
+                <hr>
+                <div id="product-features text-center">
+                    @foreach($product_features->chunk(3) as $features)
+                        <div class="row">
+                            @foreach($features as $feature)
+                                <div class="col-md-12">
+                                    <dt>
+                                        {{ $feature->feature }}
+                                        <span class="pull-right">
                                                 <i class="fa {{ $feature->status == 1 ? 'fa-check' : 'fa-check' }}"></i>
                                             </span>
-                                        </dt>
-                                        <dd>{{ $feature->details }}</dd>
-                                    </div>
-                                @endforeach
-                            </div>
-                        @endforeach
-                    </div>
+                                    </dt>
+                                    <dd>{{ $feature->details }}</dd>
+                                </div>
+                            @endforeach
+                        </div>
+                    @endforeach
                 </div>
             </div>
-
-            @if($product->coming_soon == 0)
-                <div class="row" id="button">
-                    <div class="col-lg-8 col-lg-offset-2">
-                        <a href="{{ route('app.create', ['app' => str_slug($product->title)]) }}"
-                           class="btn btn-default btn-lg pull-right" data-toggle="tooltip"
-                           title="Create {{ $product->title }}">
-                            Get Started
-                            <i class="fa fa-chevron-circle-right"></i>
-                        </a>
-                    </div>
-                </div>
-            @endif
         </div>
-    </section>
 
-    <p><br><br><br></p>
-
-    @include('partials.footers.default')
+        @if($product->coming_soon == 0)
+            <div class="row" id="button">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <a href="{{ route('app.create', ['app' => str_slug($product->title)]) }}"
+                       class="btn btn-default btn-lg pull-right" data-toggle="tooltip"
+                       title="Create {{ $product->title }}">
+                        Get Started
+                        <i class="fa fa-chevron-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+        @endif
+    </div>
 
 @endsection
--- resources/views/home/services.blade.php
+++ resources/views/home/services.blade.php
@@ -4,126 +4,121 @@
     App & Services
 @endsection
 
-@section('styles')
-    {{--<link href="{{ url('css/v1/home.css') }}" rel="stylesheet">--}}
-    @endsection
+@section('content')
 
-    @section('content')
+    <section class="bg-white" id="services">
+        <div class="row text-center">
+            <div class="col-lg-12">
+                <h2 class="section-heading">Apps</h2>
+                <hr class="star-primary">
 
-    @include('partials')
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h3 class="text-center">Live Apps</h3>
+                        <hr style="width: 15%; border-top: 5px solid #16A085;">
 
-    <section class="bg-white box" id="services">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h2 class="section-heading">Apps & Services</h2>
-                </div>
-            </div>
+                        <div class="row text-center" id="apps">
+                            @forelse($app_products as $app)
+                                <div class="{{ count($app_products) < 3 ? 'col-md-6' : 'col-md-4' }}">
+                                    <div class="clearfix app-header">
+                                        <div class="fa-stack fa-4x">
+                                            <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                                            <i class="fa {{ $app->icon }} fa-stack-1x fa-inverse"></i>
+                                        </div><!-- /icon -->
+                                        <h4>{{ $app->title }}
+                                            @if(!$app->mode == 1)
+                                                <small data-toggle="tooltip"
+                                                       title="{{ AppModeInfo($app->mode) }}">
+                                                    <span class="label label-warning">Beta</span>
+                                                </small>
+                                            @endif
+                                        </h4><!-- /title -->
+                                    </div><!-- /header -->
+                                    <hr>
 
-            <h3 class="page-header"><i class="fa fa-laptop"></i> Apps</h3>
+                                    <div class="clearfix summary">
+                                        {!! $app->summary !!}
+                                    </div><!-- /summary -->
+                                    <hr>
 
-            <div class="row text-center" id="apps">
-                @forelse($app_products as $app)
-                    <div class="{{ count($app_products) < 3 ? 'col-md-6' : 'col-md-4' }}">
-                        <header class="clearfix">
-                            <span class="fa-stack fa-4x">
-                                <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                                <i class="fa {{ $app->icon }} fa-stack-1x fa-inverse"></i>
-                            </span>
-                            <!-- /icon -->
-                            <h4 class="service-heading">{{ $app->title }}
-                                @if(!$app->mode == 1)
-                                    <small data-toggle="tooltip"
-                                           title="{{ AppModeInfo($app->mode) }}">
-                                        <span class="label label-warning">Beta</span>
-                                    </small>
-                                @endif
-                            </h4>
-                            <!-- /title -->
-                        </header>
-                        <!-- /header -->
-                        <hr>
-                        <section class="clearfix summary">
-                            {!! $app->summary !!}
-                        </section>
-                        <!-- /summary -->
-                        <hr>
+                                    <div class="text-muted">
+                                        <a href="{{ route('service', ['id' => $app->id]) }}" class="btn btn-default">
+                                            Learn more <i class="fa {{ $app->icon }}"></i>
+                                        </a>
 
-                        <div class="text-muted">
-                            <a href="{{ route('service', ['id' => $app->id]) }}" class="btn btn-default">
-                                Learn more <i class="fa {{ $app->icon }}"></i>
-                            </a>
+                                        <a href="{{ route('app.create', ['app' => str_slug($app->title)]) }}"
+                                           class="btn btn-primary" data-toggle="tooltip"
+                                           title="Add {{ $app->title }}">
+                                            Get Started
+                                            <i class="fa fa-chevron-circle-right"></i>
+                                        </a>
+                                    </div><!-- /links -->
 
-                            <a href="{{ route('app.create', ['app' => str_slug($app->title)]) }}"
-                               class="btn btn-primary" data-toggle="tooltip"
-                               title="Add {{ $app->title }}">
-                                Get Started
-                                <i class="fa fa-chevron-circle-right"></i>
-                            </a>
-                        </div>
-                        <!-- /links -->
+                                    @if($loop->count > 1 && $loop->last)
+                                        <hr class="hidden-md hidden-lg hidden-sm">
+                                    @endif
+                                </div><!-- /.col-md-* -->
+                                <p class="visible-xs"></p>
 
-                        @if($loop->count > 1 && $loop->last)
-                            <hr class="hidden-md hidden-lg hidden-sm">
-                        @endif
-                    </div><!-- /.col-md-* -->
-                    <p></p>
+                            @empty
+                                <p class="text-muted">Sorry, some error occured. Reload page.</p>
+                            @endforelse
+                        </div><!-- /.row#apps -->
+                    </div><!-- /.col-lg-12 -->
+                </div><!-- /.row -->
 
-                @empty
-                    <p class="text-muted">Sorry, some error occured. Reload page.</p>
-                @endforelse
-            </div><!-- /.row#apps -->
+                @if($apps_coming->count())
+                    <div class="section-top"></div>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h3 class="text-center">Coming Soon</h3>
+                            <hr style="width: 15%; border-top: 5px solid #3494DB;">
+                            <div class="row text-center" id="apps-soon">
+                                @forelse($apps_coming as $app)
+                                    <div class="{{ count($apps_coming) < 3 ? 'col-md-6' : 'col-md-4' }}">
+                                        <div class="clearfix app-header">
+                                            <div class="fa-stack fa-4x">
+                                                <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                                                <i class="fa {{ $app->icon }} fa-stack-1x fa-inverse"></i>
+                                            </div><!-- /icon -->
 
-            <h3 class="page-header"><i class="fa fa-hourglass-half"></i> Coming Soon</h3>
+                                            <h4 class="service-heading">{{ $app->title }}
+                                                @if($app->mode == 0)
+                                                    <span class="label label-warning">Coming Soon</span>
+                                                @endif
+                                            </h4><!-- /title -->
+                                        </div><!-- /header -->
+                                        <hr>
 
-            <div class="row text-center" id="apps-soon">
-                @forelse($apps_coming as $app)
-                    <div class="{{ count($apps_coming) < 3 ? 'col-md-6' : 'col-md-4' }}">
-                        <header>
-                            <span class="fa-stack fa-4x">
-                                <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                                <i class="fa {{ $app->icon }} fa-stack-1x fa-inverse"></i>
-                            </span>
-                            <!-- /icon -->
+                                        <div class="clearfix summary">
+                                            {!! $app->summary !!}
+                                        </div><!-- /summary -->
+                                        <hr>
 
-                            <h4 class="service-heading">{{ $app->title }}
-                                @if($app->mode == 0)
-                                    <span class="label label-warning">Coming Soon</span>
-                                @endif
-                            </h4>
-                            <!-- /title -->
-                        </header>
-                        <!-- /header -->
-                        <hr>
+                                        <p class="text-muted">
+                                            <a href="{{ route('service', ['id' => $app->id]) }}"
+                                               class="btn btn-default">
+                                                Learn more <i class="fa {{ $app->icon }}"></i>
+                                            </a>
+                                        </p>
+                                        <!-- /links -->
 
-                        <section class="clearfix summary">
-                            {!! $app->summary !!}
-                        </section>
-                        <!-- /summary -->
-                        <hr>
+                                        @if($loop->count > 1 && $loop->last)
+                                            <hr class="hidden-md hidden-lg hidden-sm">
+                                        @endif
 
-                        <p class="text-muted">
-                            <a href="{{ route('service', ['id' => $app->id]) }}" class="btn btn-default">
-                                Learn more <i class="fa {{ $app->icon }}"></i>
-                            </a>
-                        </p>
-                        <!-- /links -->
+                                    </div><!-- /.col-md-* -->
+                                    <p></p>
 
-                        @if($loop->count > 1 && $loop->last)
-                            <hr class="hidden-md hidden-lg hidden-sm">
-                        @endif
-
-                    </div><!-- /.col-md-* -->
-                    <p></p>
-
-                @empty
-                    <p class="text-muted">Sorry, some error occured. Reload page.</p>
-                @endforelse
-            </div><!-- /.row#apps-soon -->
-
-        </div><!-- /.container -->
+                                @empty
+                                    <p class="text-muted">Sorry, some error occured. Reload page.</p>
+                                @endforelse
+                            </div><!-- /.row#apps-soon -->
+                        </div><!-- /.col-lg-12 -->
+                    </div><!-- /.row -->
+                @endif
+            </div><!-- /.col-lg-12 -->
+        </div><!-- /.row -->
     </section><!-- /#services -->
-
-    @include('partials.footers.default')
 
 @endsection
