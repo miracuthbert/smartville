2ad23fa3da259aa324b5ec0305034646316ec731
miracuthbert <miracuthbert@gmail.com>
2017-08-21 15:56:58 +0300

    Update CompanyAppPolicy
    Updated authorization functionality in default
    Added touch function to handle general authorization

--- app/Policies/CompanyAppPolicy.php
+++ app/Policies/CompanyAppPolicy.php
@@ -14,7 +14,7 @@ class CompanyAppPolicy
      * Determine whether the user can view the companyApp.
      *
      * @param  \App\User $user
-     * @param  \App\CompanyApp $companyApp
+     * @param CompanyApp $companyApp
      * @return mixed
      */
     public function view(User $user, CompanyApp $companyApp)
@@ -28,41 +28,50 @@ class CompanyAppPolicy
      * Determine whether the user can create companyApps.
      *
      * @param  \App\User $user
+     * @param CompanyApp $companyApp
      * @return mixed
      */
-    public function create(User $user)
+    public function create(User $user, CompanyApp $companyApp)
     {
-        //
+        return $this->touch($user, $companyApp);
     }
 
     /**
      * Determine whether the user can update the companyApp.
      *
      * @param  \App\User $user
-     * @param  \App\CompanyApp $companyApp
+     * @param CompanyApp $companyApp
      * @return mixed
      */
     public function update(User $user, CompanyApp $companyApp)
     {
-        $app = $user->companies()->where('company_app_id', $companyApp->id)->first();
-
-        $admin = $user->companyAppAdmin() != null ? $user->companyAppAdmin->admin : '';
-
-        return $app->company_app_id === $companyApp->id;
+        return $this->touch($user, $companyApp);
     }
 
     /**
      * Determine whether the user can delete the companyApp.
      *
      * @param  \App\User $user
-     * @param  \App\CompanyApp $companyApp
+     * @param CompanyApp $companyApp
      * @return mixed
      */
     public function delete(User $user, CompanyApp $companyApp)
     {
+        return $this->touch($user, $companyApp);
+    }
+
+    /**
+     * Determine whether the user can take action on the companyApp.
+     *
+     * @param  \App\User $user
+     * @param CompanyApp $companyApp
+     * @return mixed
+     */
+    public function touch(User $user, CompanyApp $companyApp)
+    {
         $app = $user->companies()->where('company_app_id', $companyApp->id)->first();
 
-        $admin = $user->companyAppAdmin() != null ? $user->companyAppAdmin->admin : '';
+        $admin = $user->companyAppAdmin() ? $user->companyAppAdmin->admin : '';
 
         return $app->company_app_id === $companyApp->id;
     }
