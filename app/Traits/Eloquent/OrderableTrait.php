d37d4423bdedf850c92296f531cdd1105df28d39
miracuthbert <miracuthbert@gmail.com>
2017-08-21 21:22:50 +0300

    Add OrderableTrait
    Holds functions to order models

---
+++ app/Traits/Eloquent/OrderableTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Traits\Eloquent;
+
+
+trait OrderableTrait
+{
+
+    /**
+     * Order model results by latest first
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeLatestFirst($query)
+    {
+        return $query->orderBy('created_at', 'DESC');
+    }
+
+    /**
+     * Order model results by latest first
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeLatestDelete($query)
+    {
+        return $query->orderBy('deleted_at', 'DESC');
+    }
+}
