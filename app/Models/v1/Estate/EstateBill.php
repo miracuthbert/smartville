09c0fd10806c600b9df5f959cf54a93c8eeff87d
miracuthbert <miracuthbert@gmail.com>
2017-08-22 22:32:26 +0300

    Update EstateBill Model
    Added OrderableTrait
    Added scope to check to query status

--- app/Models/v1/Estate/EstateBill.php
+++ app/Models/v1/Estate/EstateBill.php
@@ -4,14 +4,37 @@ namespace App\Models\v1\Estate;
 
 use App\Models\v1\Company\CompanyApp;
 use App\Models\v1\Tenant\TenantBill;
+use App\Traits\Eloquent\OrderableTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EstateBill extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, OrderableTrait;
 
-    protected $dates = ['created_at, updated_at, deleted_at'];
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    /**
+     * Get active bill service(s).
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeIsActive($query)
+    {
+        return $query->where('status', true);
+    }
+
+    /**
+     * Get not active bill service(s).
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeIsNotActive($query)
+    {
+        return $query->where('status', false);
+    }
 
     /**
      * Get Bill App
