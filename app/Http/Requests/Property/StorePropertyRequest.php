8ef3c38db68d9f86707ab6e31523224b370cca2e
miracuthbert <miracuthbert@gmail.com>
2017-09-04 11:06:16 +0300

    Update StorePropertyFormRequest
    Fixed namespace

--- app/Http/Requests/Property/StorePropertyRequest.php
+++ app/Http/Requests/Property/StorePropertyRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Estate\Rental\Property;
+namespace App\Http\Requests\Property;
 
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -24,17 +24,16 @@ class StorePropertyRequest extends FormRequest
     public function rules()
     {
         return [
-            '_app' => 'required|integer|exists:company_apps,id',
             'title' => 'required|min:2|max:255',
             'summary' => 'required',
             'description' => 'nullable|min:3|max:2500',
-            'type' => 'required|integer|exists:categories,id',
-            'group' => 'nullable|integer|exists:estate_groups,id',
+            'type' => 'required|exists:categories,id',
+            'group' => 'nullable|exists:estate_groups,id',
             'size' => 'required|numeric',
             'interval' => 'required|integer',
             'price' => 'required|numeric',
             '_rentable' => 'required|boolean',
-            'amenity.*' => 'integer',
+            'amenity.*' => 'nullable|exists:amenities,id',
             'feature.*' => 'string',
             'details.*' => 'string',
             'value.*' => 'numeric',
