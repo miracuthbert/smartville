4e67778a382bf6ef9c7f72c0f4552ab3b9597b01
miracuthbert <miracuthbert@gmail.com>
2017-09-05 09:49:17 +0300

    Update PropertyFeature model and form request
    Added OrderableTrait and overrided dates field in model
    Added form request to handle property features validation

--- app/Models/v1/Property/PropertyFeature.php
+++ app/Models/v1/Property/PropertyFeature.php
@@ -2,12 +2,20 @@
 
 namespace App\Models\v1\Property;
 
+use App\Traits\Eloquent\OrderableTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PropertyFeature extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, OrderableTrait;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 
     /**
      * Get Feature Property
